93023c25828e11e52742c8ce2bfea8e345f00024
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-04-07 20:36:48 +0400

    feat: task 05

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,18 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const st = String(n).split('');
+  let max = 0;
+  let s = st.slice(0);
+
+  for (let i = 0; i < s.length; i++) {
+    s.splice(s.indexOf(s[i]), 1);
+    const sn = +(s.join(''));
+    if (sn > max) max = sn;
+    s = st.slice(0);
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
