7f2085d174d4241223a9d53aa3c76337da86e587
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-04-07 19:28:25 +0400

    feat: task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,28 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = matrix.map((e) => e.map((i) => { if (i) return -1; return 0; }));
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      if (result[i][j] !== -1) {
+        let u = 0;
+        if ((i - 1 >= 0) && (result[i - 1][j] === -1)) u += 1;
+        if ((i + 1 < result.length) && (result[i + 1][j] === -1)) u += 1;
+        if ((j - 1 >= 0) && (result[i][j - 1] === -1)) u += 1;
+        if ((j + 1 < result[i].length) && (result[i][j + 1] === -1)) u += 1;
+
+        if ((i - 1 >= 0) && (j - 1 >= 0) && (result[i - 1][j - 1] === -1)) u += 1;
+        if ((i - 1 >= 0) && (j + 1 < result[i].length) && (result[i - 1][j + 1] === -1)) u += 1;
+        if ((i + 1 < result.length) && (j - 1 >= 0) && (result[i + 1][j - 1] === -1)) u += 1;
+        if ((i + 1 < result.length) && (j + 1 < result[i].length)
+        && (result[i + 1][j + 1] === -1)) u += 1;
+        result[i][j] = u;
+      }
+    }
+  }
+  const res = result.map((e) => e.map((i) => { if (i === -1) return 1; return i; }));
+  return res;
 }
 
 module.exports = minesweeper;
