3a12e023269d5e2c0bdb70287e54be640780a161
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-04-07 15:04:44 +0400

    feat: task 01

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,17 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let summ = 0;
+  matrix[0].forEach((item) => { summ += item; });
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1][j] !== 0) {
+        summ += matrix[i][j];
+      }
+    }
+  }
+  return summ;
 }
 
 module.exports = getMatrixElementsSum;
