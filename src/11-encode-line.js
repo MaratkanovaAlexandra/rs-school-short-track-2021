b7e5d9580bf16b75e66e8f2d8b7ed1c3437de6a8
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-04-09 12:56:48 +0400

    feat: task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const ind = [];
+  const set = str.split('');
+  for (let i = 0; i < set.length; i++) {
+    const j = i + 1;
+    let ingex = 1;
+    while (set[i] === set[j]) {
+      ingex++;
+      set.splice(j, 1);
+    }
+    ind.push(ingex);
+  }
+  let result = '';
+  for (let i = 0; i < set.length; i++) {
+    if (ind[i] !== 1) {
+      result += String(ind[i]);
+    }
+    result += set[i];
+  }
+  return result;
 }
 
 module.exports = encodeLine;
